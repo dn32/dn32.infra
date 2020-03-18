9c39ec2ca819afb0953399890e0dfbda17dc10f7
marcelo.souza <Marcelo>
2020-03-18 16:04:14 -0300

    Correção

---
+++ dn32.infra/excecoes/DesenvolvimentoIncorretoException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace dn32.infra.excecoes
+{
+    public class DesenvolvimentoIncorretoException : Exception
+    {
+        public DesenvolvimentoIncorretoException(string mensagem) : base(mensagem)
+        {
+        }
+    }
+}
